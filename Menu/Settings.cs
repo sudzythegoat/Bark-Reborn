429da2ed0b75fa209e061cb1472ee3c24e2288e0
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-18 15:01:32 -0500

    Update Settings.cs

--- Menu/Settings.cs
+++ Menu/Settings.cs
@@ -9,25 +9,25 @@ namespace StupidTemplate
         public static ExtGradient backgroundColor = new ExtGradient{isRainbow = true};
         public static ExtGradient[] buttonColors = new ExtGradient[]
         {
-            new ExtGradient{colors = GetSolidGradient(Color.black) }, // Disabled
-            new ExtGradient{isRainbow = true} // Enabled
+            new ExtGradient{colors = GetSolidGradient(Color.brown) }, // Disabled
+            new ExtGradient{{colors = GetSolidGradient(Color.grey) } // Enabled
         };
         public static Color[] textColors = new Color[]
         {
-            Color.white, // Disabled
-            Color.white // Enabled
+            Color.grey, // Disabled
+            Color.black // Enabled
         };
 
-        public static Font currentFont = (Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font);
+        public static Font currentFont = (Resources.GetBuiltinResource(typeof(Font), "Lucida Console.ttf") as Font);
 
         public static bool fpsCounter = true;
-        public static bool disconnectButton = true;
-        public static bool rightHanded = false;
+        public static bool disconnectButton = false;
+        public static bool rightHanded = true;
         public static bool disableNotifications = false;
 
         public static KeyCode keyboardButton = KeyCode.Q;
 
-        public static Vector3 menuSize = new Vector3(0.1f, 1f, 1f); // Depth, Width, Height
+        public static Vector3 menuSize = new Vector3(0.1f, 1.1f, 1f); // Depth, Width, Height
         public static int buttonsPerPage = 8;
     }
 }
