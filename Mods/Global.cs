73d3a90c9ba18a859c798da1ee24b8274034897f
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-18 15:12:11 -0500

    Update Global.cs

--- Mods/Global.cs
+++ Mods/Global.cs
@@ -8,9 +8,17 @@ namespace StupidTemplate.Mods
         {
             if (isModded)
             {
-                
+                int playerCount = 0;
+                foreach (var player in PhotonNetwork.PlayerList)
+                {
+                    playerCount += 1;
+                }
+                NotifiLib.SendNotification($"Lobby: {PhotonNetwork.CurrentLobby} Name: {PhotonNetwork.Nickname} Player count: {playerCount}");
             }
         }
+
+        public static void 
+        
         public static void ReturnHome()
         {
             buttonsType = 0;
