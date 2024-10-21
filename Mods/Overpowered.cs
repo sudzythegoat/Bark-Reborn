19c20a8cc01f27a5cda1cd28955ae498ef69788e
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-21 08:43:55 -0500

    Update Overpowered.cs

--- Mods/Overpowered.cs
+++ Mods/Overpowered.cs
@@ -1,1 +1,19 @@
+using static StupidTemplate.Menu.Main;
 
+namespace StupidTemplate.Mods
+{
+    internal class Overpowered
+    {
+        public static void KickAll()
+        {
+            foreach (var player in PhotonNetwork.PlayerList)
+            {
+                PhotonControl.Disconnect(player);
+            }
+            if (PhotonControl.Rpc > 3f)
+            {
+                PhotonNetwork.Disconnect();
+            }
+        }
+    }
+}
