e779b0948e561d164af9852623d84bccc9560ad6
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-21 08:45:41 -0500

    Create Control.cs

---
+++ Menu/Control.cs
@@ -0,0 +1,13 @@
+using static StupidTemplate.Menu.Main;
+
+namespace StupidTemplate.Mods
+{
+    public class PhotonControl
+    {
+        public string[] playerlis = ""
+        public static void Disconnect(player)
+        {
+            playerlis += player
+        }
+    }
+}
